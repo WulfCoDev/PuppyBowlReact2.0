50fc544390990562cc1f20f0c0118df80cad7a00
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-08-07 15:23:57 -0700

    delete logic error fix

--- src/API/ajaxHelpers.js
+++ src/API/ajaxHelpers.js
@@ -37,7 +37,7 @@ export const fetchSinglePlayer = async (playerId) => {
 
 export const deletePlayer = async (playerId) => {
     try {
-        const response = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-ET-WEB-PT-D/players' + playerId, {
+        const response = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-ET-WEB-PT-D/players/' + playerId, {
             method: 'DELETE',
         });
         if (!response.ok) {
